c8172f17a8a2d1f3658e0c1447e0813bb1c3116e
Simon Cornelius <simonjoecornelius@gmail.com>
2024-11-30 13:58:46 +0100

    [SDL] basic example render

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <SDL3/SDL.h>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("Initialized SDL %d\n", SDL_GetVersion());
+
+    SDL_Window* window = SDL_CreateWindow("tinyrenderer", 800, 600, 0);
+    if (window == nullptr) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == nullptr) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    bool isRunning = true;
+    while (isRunning) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_EVENT_QUIT:
+                    isRunning = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0X01, 0X82, 0X3f, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
